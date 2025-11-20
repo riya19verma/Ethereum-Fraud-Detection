61a5385f7959ca05788e8ce3eb11e51eba8dd749
riya19verma <verma.riya1910@gmail.com>
2025-11-20 10:48:49 +0530

    update repo

---
+++ comparision.py
@@ -0,0 +1,67 @@
+import numpy as np
+import pandas as pd
+import seaborn as sns
+import matplotlib.pyplot as plt
+from sklearn.metrics import accuracy_score
+import zipfile
+
+from pycaret.classification import *
+from pycaret.classification import ClassificationExperiment
+
+zip_file_path = 'dataset-1.zip'
+with zipfile.ZipFile(zip_file_path, 'r') as zip_ref:
+    zip_ref.extractall()
+
+df = pd.read_csv('transaction_dataset.csv')
+df = df.drop(columns=['Index', 'Address','Unnamed: 0'])
+print(df.head())
+common_elements_list =['FLAG','Time Diff between first and last (Mins)', 'Received Tnx', 'avg val received', ' Total ERC20 tnxs']
+
+original_data = df
+new_data = df
+for i in df.columns:
+  if i not in common_elements_list:
+    new_data = new_data.drop(columns=[i], axis=1)
+    
+#cat_data = df
+#cat_ele = ['FLAG','Time Diff between first and last (Mins)','Received Tnx','Unique Received From Addresses','avg val received','min val sent','Total ERC20 tnxs','ERC20 total Ether received','ERC20 uniq rec contract addr']
+#for i in df.columns:
+#  if i not in cat_ele:
+#    cat_data = cat_data.drop(columns=[i], axis=1)
+
+lgbm_data = df
+lgbm_ele = ['FLAG','Avg min between received tnx','Time Diff between first and last (Mins)','Sent tnx','Received Tnx','Unique Received From Addresses','avg val received', 'min val sent','total ether balance','Total ERC20 tnxs',' ERC20 total Ether received',' ERC20 total ether sent',' ERC20 min val rec']
+for i in df.columns:
+  if i not in lgbm_ele:
+    lgbm_data = lgbm_data.drop(columns=[i], axis=1)
+print(lgbm_data.shape)
+          
+print("=============================  ORIGINAL DATASET  =============================")
+bin_set_og = setup(original_data, target = 'FLAG', session_id = 123, fix_imbalance = True, fix_imbalance_method = 'SMOTE')
+exp = ClassificationExperiment()
+exp.setup(original_data, target = 'FLAG', session_id = 123, fix_imbalance = True, fix_imbalance_method = 'SMOTE')
+best_og = compare_models()
+xg = create_model('xgboost')
+
+print("================================  NEW DATASET  ================================")
+bin_set_new = setup(new_data, target = 'FLAG', session_id = 123, fix_imbalance = True, fix_imbalance_method = 'SMOTE')
+exp = ClassificationExperiment()
+exp.setup(new_data, target = 'FLAG', session_id = 123, fix_imbalance = True, fix_imbalance_method = 'SMOTE')
+best_new = compare_models()
+xg = create_model('xgboost')
+
+#print("=============================  CATBOOST DATASET  =============================")
+#bin_set_cat = setup(cat_data, target = 'FLAG', session_id = 123)
+#exp = ClassificationExperiment()
+#exp.setup(cat_data, target = 'FLAG', session_id = 123)
+#best_cat = compare_models()
+# plot confusion matrix
+#plot_model(best_cat, plot = 'confusion_matrix')
+
+print("=============================  LightGBM DATASET  =============================")
+bin_set_lg = setup(lgbm_data, target = 'FLAG', session_id = 123, fix_imbalance = True, fix_imbalance_method = 'SMOTE')
+exp = ClassificationExperiment()
+exp.setup(lgbm_data, target = 'FLAG', session_id = 123, fix_imbalance = True, fix_imbalance_method = 'SMOTE')
+best_lg = compare_models()
+xg = create_model('xgboost')
+
